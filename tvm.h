5bca1b22b2168defe7a6239bf94e5c74aeb69991
Vekolyram <vekooolyyyram@outlook.com>
2025-02-21 18:32:54 +0800

    base

--- tvm.h
+++ tvm.h
@@ -1,9 +1,8 @@
 #pragma once
 #include <cstdint>
 #include <stack>
-constexpr int Regnum = 32;
-//16 = ret
-enum OPCode {
+
+enum TiOpCode {
 	ropt,
 	rjmp,
 	rhalt,
@@ -17,41 +16,35 @@ enum OPCode {
 	gout,
 	call
 };
-
-struct Instruction {
+enum TiOpType {
+	add,
+	sub,
+	mult,
+	div
+};
+struct TiStruct {};
+struct TiInstruction {
 public:
 	uint16_t instruction : 32;
 };
-struct StackInstruction : Instruction {
-public:
-	uint8_t opcode : 8;
-	uint16_t address;
-};
-class TVM {
+class TiVM {
 	int pc;
 	bool running;
-	int regs[16];
-	Instruction* currentInstruction;
-	Instruction* lastInstruction;
+	TiInstruction* currentInstruction;
+	TiInstruction* lastInstruction;
+	std::stack<TiStruct> structstack;
 	std::stack<int> callstack;
 	std::istream* input;
     std::ostream* output;
 public:
-	TVM() : regs{ -1 }, pc(0), running(false) {};
-	void parseCode(Instruction* inst);
-	void execrmov(int r1, int r2);
-	void execradd(int r1, int r2, int r3);
-	void execrsub(int r1, int r2, int r3);
-	void execrmult(int r1, int r2, int r3);
-	void execrdiv(int r1, int r2, int r3);
+	TiVM() : pc(0), running(false) {};
+	void parseCode(TiInstruction* inst);
+	void execropt(int address,);
 	void execrjmp(int address);
-	void execrjz(int r1, int address);
 	void execrout(int r1);
 	void execrin(int r1);
 	void execrhalt();
 	void execrret();
-	void execrload(int r1, int address);
-	void execrstore(int r1, int address);
 	void execlif(int r1, int address);
 	void execlwhile(int r1, int address);
 	void execlcodeblock(int address);
